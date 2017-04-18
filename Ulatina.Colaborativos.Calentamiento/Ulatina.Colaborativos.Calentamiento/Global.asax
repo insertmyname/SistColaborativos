e75e78a5b4f7c3d0b00005511f92be918ced94ae
Geannie Sharel Solis Carvajal <ge_shareliz@hotmail.es>
2017-04-18 16:29:46 -0600

    Tres soluciones creadas. Sin ningún método o modificación.

---
+++ Ulatina.Colaborativos.Calentamiento/Ulatina.Colaborativos.Calentamiento/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ulatina.Colaborativos.Calentamiento.Global" Language="C#" %>
---
+++ Ulatina.Colaborativos.Calentamiento/Ulatina.Colaborativos.Operaciones.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ulatina.Colaborativos.Operaciones.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ulatina.Colaborativos.Operaciones.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7fdc38bb-1cb6-4b3d-9c4a-7b752771966d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
